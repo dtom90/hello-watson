e6f06c9d5b3c1de16941b3fad9d360d5419a3893
David Thomason <dtom90@users.noreply.github.com>
2020-09-29 18:50:18 -0600

    Implemented basic chat interface

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,37 @@
 <script>
-	export let name;
+  import WatsonAssistant from './WatsonAssistant.svelte';
+
+  export let title = 'Hello Watson!';
+  export let noTitle = false;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    {#if !noTitle}
+        <h1>{title}</h1>
+    {/if}
+    <div class="chat-area">
+        <WatsonAssistant/>
+    </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        margin: 0 auto;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: blue;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    .chat-area {
+        flex: 1;
+    }
 </style>
---
+++ src/WatsonAssistant.svelte
@@ -0,0 +1,46 @@
+<script>
+  let messages = [
+    {bot: true, message: 'Hello, my name is Watson!'},
+    {bot: true, message: 'I can answer simple questions'},
+    {user: true, message: 'How are you today?'},
+    {bot: true, message: 'I am doing well, thank you for asking!'}
+  ];
+</script>
+
+<div class="chat-container">
+    {#each messages as {bot, message}}
+        <div class="message-container {bot ? 'bot' : 'user'}-message">
+            <span class="message">{message}</span>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .chat-container {
+        background-color: lightgray;
+        width: 100%;
+        height: 100%;
+    }
+
+    .message-container {
+        padding: 10px 10px 0 10px;
+        display: flex;
+    }
+
+    .message {
+        padding: 10px;
+        border-radius: 10px;
+    }
+
+    .bot-message > .message {
+        background-color: lightgreen;
+    }
+
+    .user-message {
+        justify-content: right;
+    }
+
+    .user-message > .message {
+        background-color: #00B4A0;
+    }
+</style>
