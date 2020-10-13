89738e4e2dbbb805b15451af410fbbd081ea915f
David Thomason <dtom90@users.noreply.github.com>
2020-10-12 21:56:56 -0600

    Split server & UI logic

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const assistantServer = require('./server');
 const polkaApi = require('./api');
-const svelteChatArea = require('./src/ChatArea.svelte');
-const svelteAssistantApp = require('./src/AssistantApp.svelte');
 
 module.exports = {
   assistantServer,
   polkaApi,
-  svelteChatArea,
-  svelteAssistantApp
 };
