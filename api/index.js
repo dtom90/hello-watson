54a3a947adf8a16cd0707295dd55ed6c2650cf4b
David Thomason <dtom90@users.noreply.github.com>
2020-11-10 20:39:55 -0700

    Separated server from invocation Fixed impersistence of sessionId

--- api/index.js
+++ api/index.js
@@ -7,9 +7,10 @@ const api = polka()
   .use(bodyParser.json());
 
 api.post('/message', async (req, res) => {
-  const {sessionId, text} = req.body;
+  let sessionId, text, result;
+  ({sessionId, text} = req.body);
   try {
-    const {result} = await sendMessage(sessionId, {text});
+    ({sessionId, result} = await sendMessage(sessionId, {text}));
     send(res, 200, {sessionId, ...result});
   } catch (error) {
     send(res, 502, {error: error.message});
---
+++ assistantServer.js
@@ -0,0 +1,28 @@
+const polka = require('polka');
+const sirv = require('sirv');
+const compression = require('compression');
+
+const api = require('./api');
+
+// Init `sirv` handler
+const assets = sirv('public', {
+  dev: process.argv[2] === '--dev',
+  maxAge: 31536000, // 1Y
+  immutable: true
+});
+
+const assistantServer = polka()
+  .use(compression(), assets)
+  .use('/api', api);
+
+const handler = (port) => {
+  return (err) => {
+    if (err) throw err;
+    console.log('> Ready at http://localhost:' + port); // eslint-disable-line no-console
+  };
+};
+
+module.exports = {
+  assistantServer,
+  handler
+};
